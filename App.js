44c6c8c0cc78d52027ee377a53284e4842891120
steve-moretz <sm.steve.moretz@gmail.com>
2022-04-02 13:13:08 +0430

    New

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from "react";
+import * as Updates from "expo-updates";
 
 export default function App() {
+  const log = async (object) => {
+    console.log(object);
+    await fetch("https://eo7smyw28ydqjai.m.pipedream.net",{
+      method : "POST",
+      body : JSON.stringify(object)
+    })
+    setLogText(JSON.stringify(object))
+  }
+
+  const [logText, setLogText] = useState("");
+  useEffect(()=>{
+    (async () => {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        await log(update);
+        if (update.isAvailable) {
+          await Updates.fetchUpdateAsync();
+          await log({
+            msg : "updated!! "
+          });
+          await Updates.reloadAsync();
+        }
+      } catch (e) {
+        await log({
+          exception : "exception : " +  e.toString()
+        });
+      } finally {
+        await log(Updates.manifest);
+      }
+    })()
+  },[]);
   return (
     <View style={styles.container}>
-      <Text>This is the native app..</Text>
+      <Text>This is the OTA!</Text>
+      <Text>{logText}</Text>
       <StatusBar style="auto" />
     </View>
   );
